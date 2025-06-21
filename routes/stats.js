2fedf88dfc956b865df2b4762c8337276f19f976
Jakub Krać <jakubkrac0123@gmail.com>
2025-06-21 22:01:37 +0200

    Dodanie strony błędu.

--- routes/stats.js
+++ routes/stats.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const stats_controller = require("../controllers/statsController");
-/* GET stats */
 
 router.get("/", stats_controller.stats_list);
 
