02526fb29a2fffe06ac051d8d3e796c076c7ab5c
Jakub Krać <jakubkrac0123@gmail.com>
2025-06-21 22:18:39 +0200

    Dodanie admin_panel.

--- routes/admin_panel.js
+++ routes/admin_panel.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const authenticateJWT = require('../middlewares/authenticateJWT');
+const User = require('../models/user');
+const Gallery = require('../models/gallery');
+const Image = require('../models/image');
 
 // Middleware sprawdzający rolę admina
 function authorizeAdmin(req, res, next) {
@@ -11,8 +14,27 @@ function authorizeAdmin(req, res, next) {
   }
 }
 
-router.get('/', authenticateJWT, authorizeAdmin, function(req, res, next) {
-  res.render('admin-panel', { title: 'xd' });
+router.get('/', authenticateJWT, authorizeAdmin, async (req, res, next) => {
+  try {
+    // Pobierz wszystkich użytkowników
+    const users = await User.find().lean();
+
+    // Pobierz wszystkie galerie z dołączonym użytkownikiem (populate user)
+    const galleries = await Gallery.find().populate('user').lean();
+
+    // Do każdej galerii dołącz obrazy
+    for (const gallery of galleries) {
+      gallery.images = await Image.find({ gallery: gallery._id }).lean();
+    }
+
+    res.render('admin-panel', {
+      title: 'Panel administratora',
+      users,
+      galleries
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
