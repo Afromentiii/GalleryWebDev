1e4da6872ca8c947ab3bd3fc568ab6f52832cf2d
Jakub Krać <jakubkrac0123@gmail.com>
2026-07-25 00:30:32 +0200

    style(UI): dodanie stylizacji i frameworka bootstrap

---
+++ middlewares/authenticateJWT.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'twoj_tajny_klucz'; 
+
+function authenticateJWT(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.redirect('/login');
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; 
+    next();
+  } catch (err) {
+    return res.redirect('/login');
+  }
+}
+
+module.exports = authenticateJWT;
